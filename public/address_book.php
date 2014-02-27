7fb8d59b3e528064e4e2492b0b3316f83adacb89
JaimeVelasco <jaime@photojv.com>
2014-02-26 22:40:02 -0600

    Cleaner, more readable code.

--- public/address_book.php
+++ public/address_book.php
@@ -6,11 +6,6 @@
 	// echo "<p>POST:</p>";
 	// var_dump($_POST);
 
-
-
-
-
-
 $address_book = [];
 $filename= 'Data/address_book.csv';
 
@@ -36,9 +31,6 @@ function addItem($NewList) {
 		return $NewList;		
 		}
 		
-	
-
-
 function saveFile($NewList, $filename) {
 		$handle = fopen($filename, 'w');
 		foreach ($NewList as $fields) {
@@ -49,18 +41,12 @@ function saveFile($NewList, $filename) {
 		fclose($handle);
 	}
 	
-
 	$NewList = open_file($filename);
 	$NewList = addItem($NewList);
 	
-
 // var_dump($NewList);
 	saveFile($NewList, $filename)
 ?>	
-
-
-
-
 
 <!DOCTYPE HTML>
 <html>
@@ -81,7 +67,6 @@ function saveFile($NewList, $filename) {
   <th>Phone</th>
 </tr>
 
-
 <tr>
 <? foreach ($NewList as $key => $value) : ?> 
 	<tr>
@@ -97,51 +82,29 @@ function saveFile($NewList, $filename) {
 
 </tr>
 
-
-
-
-
-
 </table>
-
-
-
 
 <h1 align="center">Add a new entry to the Address Book</h1>
 				
 
 	    <form align="center" method="POST" enctype="multipart/form-data" action="">
 	        <p
-	            <label for="Name"></label>
 	            <input id="Name" name="Name" placeholder="Name" type="text" autofocus='autofocus' required>
-	        
 	       
-	            <label for="Address"></label>
 	            <input id="Address" name="Address" placeholder="Address" type="text" required>
 	        
-	       
-	        	<label for="City"></label>
 	            <input id="City" name="City" placeholder="City" type="text" required>
-	        
-	        
-	            <label for="State"></label>
-	            <input id="State" name="State" placeholder="State" type="text" required>
 	      
-	     
-	        	<label for="ZipCode"></label>
+	            <input id="State" name="State" placeholder="State" type="text" required>
+	       
 	            <input id="ZipCode" name="ZipCode" placeholder="ZipCode" type="text" required>
 	    
-	    
-	            <label for="Phone"></label>
 	            <input id="Phone" name="Phone" placeholder="Phone" type="text" >
 	    
-	      
 	        	<input type="submit" value="Add new entry" >
 	        </p>
 
 	    </form>
-
-
 
 </body>
 <footer>
