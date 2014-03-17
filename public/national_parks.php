53ef5d6c6ed6a668f8d02b640f2460b5b26a3509
JaimeVelasco <jaime@photojv.com>
2014-03-17 16:57:02 -0500

    Improvements in code indentation for readability

--- public/national_parks.php
+++ public/national_parks.php
@@ -61,164 +61,159 @@ if (!empty($_POST)) {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
-    <link href="assets/css/datepicker.css" rel="stylesheet">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
 
-    <title>National Parks</title>
+<title>National Parks</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="assets/css/parks.css" rel="stylesheet">
-
-
-  </head>
-
-  <body>
-
-        
+<!-- Custom styles for this template -->
+<link href="assets/css/parks.css" rel="stylesheet">
 
 
-    <div class="navbar navbar-static-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="http://photojv.com/">Home</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <!-- <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul> -->
-        </div>
-        <!--/.nav-collapse -->
-      </div>
+</head>
+
+<body>
+
+<div class="navbar navbar-static-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://photojv.com/">Home</a>
     </div>
+    <div class="collapse navbar-collapse">
+      <!-- <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul> -->
+    </div>
+    <!--/.nav-collapse -->
+  </div>
+</div>
 
-    <div class="container">
-        <? if(!empty($errorMessage)): ?> 
-        <div class="alert alert-danger" data-dismiss="alert"><?= $errorMessage; ?>
+<div class="container">
+    <? if(!empty($errorMessage)): ?> 
+    <div class="alert alert-danger" data-dismiss="alert"><?= $errorMessage; ?>
+    </div>
+<? endif; ?>
+    <div class="col-md-12">
+        <div class="form-horizontal" >
+            <form method="POST" action="national_parks.php" role="form">
+              
+              <div class="form-group">
+                 <label for="name" class="col-sm-2 control-label">Park Name</label>
+              <div class="col-sm-10">
+                 <input type="text" class="form-control" name="name" id="name" placeholder="Park Name"value="<?php if(isset($_POST['name'])){ echo $_POST['name'];}else{ echo '';}?>" required>
+              </div>
+              </div>
+             
+              <div class="form-group">
+                 <label for="location" class="col-sm-2 control-label">Location</label>
+              <div class="col-sm-10">
+                 <input type="text" class="form-control" name="location" id="location" placeholder="location" value="<?php if(isset($_POST['location'])){ echo $_POST['location'];}else{ echo '';}?>" required>
+              </div>
+              </div>
+
+              <div class="form-group">
+                 <label for="date_established" class="col-sm-2 control-label">Date Established</label>
+              <div class="col-sm-10">
+                 <input type="date" class="form-control" name="date_established" id="date_established" data-date-format="yyyy/mm/dd" placeholder="yyyy/mm/dd" value="<?php if(isset($_POST['date_established'])){ echo $_POST['date_established'];}else{ echo '';}?>" required>
+              </div>
+              </div>
+
+              <div class="form-group">
+                 <label for="area_in_acres" class="col-sm-2 control-label">Area</label>
+              <div class="col-sm-10">
+                 <input type="text" class="form-control" name="area_in_acres"
+                  id="area_in_acres" placeholder="area_in_acres" value="<?php if(isset($_POST['area_in_acres'])){ echo $_POST['area_in_acres'];}else{ echo '';}?>" required>
+              </div>
+              </div>
+
+              <div class="form-group">
+                 <label for="description" class="col-sm-2 control-label">Description</label>
+              <div class="col-sm-10">
+                 <input type="text" class="form-control" 
+                  name= "description" id="description" placeholder="Description" value="<?php if(isset($_POST['description'])){ echo $_POST['description'];}else{ echo '';}?>" required>
+              </div>
+              </div>
+
+              <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                 <button type="submit" class="btn btn-default">submit</button>
+              </div>
+              </div>
+            </form>
         </div>
-    <? endif; ?>
-        <div class="col-md-12">
-            <div class="form-horizontal" >
-                <form method="POST" action="national_parks.php" role="form">
-                  <div class="form-group">
-                     <label for="name" class="col-sm-2 control-label">Park Name</label>
-                  <div class="col-sm-10">
-                     <input type="text" class="form-control" name="name" id="name" placeholder="Park Name"value="<?php if(isset($_POST['name'])){ echo $_POST['name'];}else{ echo '';}?>" required>
-                  </div>
-                  </div>
-                  <div class="form-group">
-                     <label for="location" class="col-sm-2 control-label">Location</label>
-                  <div class="col-sm-10">
-                     <input type="text" class="form-control" name="location" id="location" placeholder="location" value="<?php if(isset($_POST['location'])){ echo $_POST['location'];}else{ echo '';}?>" required>
-                  </div>
-                  </div>
-
-                  <div class="form-group">
-                     <label for="date_established" class="col-sm-2 control-label">Date Established</label>
-                  <div class="col-sm-10">
-                     <input type="date" class="form-control" name="date_established" id="date_established" data-date-format="yyyy/mm/dd" placeholder="yyyy/mm/dd" value="<?php if(isset($_POST['date_established'])){ echo $_POST['date_established'];}else{ echo '';}?>" required>
-                  </div>
-                  </div>
-
-                  <div class="form-group">
-                     <label for="area_in_acres" class="col-sm-2 control-label">Area</label>
-                  <div class="col-sm-10">
-                     <input type="text" class="form-control" name="area_in_acres"
-                      id="area_in_acres" placeholder="area_in_acres" value="<?php if(isset($_POST['area_in_acres'])){ echo $_POST['area_in_acres'];}else{ echo '';}?>" required>
-                  </div>
-                  </div>
-
-                  <div class="form-group">
-                     <label for="description" class="col-sm-2 control-label">Description</label>
-                  <div class="col-sm-10">
-                     <input type="text" class="form-control" 
-                      name= "description" id="description" placeholder="Description" value="<?php if(isset($_POST['description'])){ echo $_POST['description'];}else{ echo '';}?>" required>
-                  </div>
-                  </div>
-
-                  <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                     <button type="submit" class="btn btn-default">submit</button>
-                  </div>
-                  </div>
-                </form>
-            </div>
-        </div>
-    </div> 
+    </div>
+</div> 
     
 
 
-    <div class="table">
-        <div class="col-md-12 table-responsive">
-            <h1>A Selection of 10 National Parks in the US</h1>
-              <table class="table table-bordered">
+<div class="table">
+    <div class="col-md-12 table-responsive">
+        <h1>A Selection of 10 National Parks in the US</h1>
+          <table class="table table-bordered">
 
-                        <thead>
-                            <tr>
-                                <th class='col-md-1'>
-                                    Park Name
-                                    <br>
-                                    <i><a href="?sortcol=name&sortorder=asc" class="fa fa-sort-desc"</i></a>
-                                    <i><a href="?sortcol=name&sortorder=desc" class="fa fa-sort-asc"</i></a>
-                                </th>    
-                                <th class='col-md-1'>
-                                    Location
-                                    <br>
-                                    <i><a href="?sortcol=location&sortorder=asc" class="fa fa-sort-desc"</i></a>
-                                    <i><a href="?sortcol=location&sortorder=desc" class="fa fa-sort-asc"</i></a>
-                                </th>
-                                <th class='col-md-2'>
-                                    Date Stablished
-                                    <br>
-                                    <i><a href="?sortcol=date_established&sortorder=asc" class="fa fa-sort-desc"</i></a>
-                                    <i><a href="?sortcol=date_established&sortorder=desc" class="fa fa-sort-asc"</i></a>
-                                </th>
-                                <th class='col-md-1'>
-                                    Area in Acres
-                                    <br>
-                                    <i><a href="?sortcol=area_in_acres&sortorder=asc" class="fa fa-sort-desc"</i></a>
-                                    <i><a href="?sortcol=area_in_acres&sortorder=desc" class="fa fa-sort-asc"</i></a>
-                                </th>
-                                <th class='col-md-6'>
-                                    Description
-                                </th>
-                            </tr>
-                        </thead>
-                       
-                      <?php while ($row = $result->fetch_assoc()): ?>
-                      <tr>  
-                        <td> <?= $row['name']; ?> </td>
-                        <td> <?= $row['location']; ?> </td>
-                        <td> <?= $row['date_established']; ?> </td>
-                        <td> <?= $row['area_in_acres']; ?> </td>
-                        <td> <?= $row['description']; ?> </td>
-                    <?php endwhile ?>  
+                    <thead>
+                        <tr>
+                            <th class='col-md-1'>
+                                Park Name
+                                <br>
+                                <i><a href="?sortcol=name&sortorder=asc" class="fa fa-sort-desc"</i></a>
+                                <i><a href="?sortcol=name&sortorder=desc" class="fa fa-sort-asc"</i></a>
+                            </th>    
+                            <th class='col-md-1'>
+                                Location
+                                <br>
+                                <i><a href="?sortcol=location&sortorder=asc" class="fa fa-sort-desc"</i></a>
+                                <i><a href="?sortcol=location&sortorder=desc" class="fa fa-sort-asc"</i></a>
+                            </th>
+                            <th class='col-md-2'>
+                                Date Stablished
+                                <br>
+                                <i><a href="?sortcol=date_established&sortorder=asc" class="fa fa-sort-desc"</i></a>
+                                <i><a href="?sortcol=date_established&sortorder=desc" class="fa fa-sort-asc"</i></a>
+                            </th>
+                            <th class='col-md-1'>
+                                Area in Acres
+                                <br>
+                                <i><a href="?sortcol=area_in_acres&sortorder=asc" class="fa fa-sort-desc"</i></a>
+                                <i><a href="?sortcol=area_in_acres&sortorder=desc" class="fa fa-sort-asc"</i></a>
+                            </th>
+                            <th class='col-md-6'>
+                                Description
+                            </th>
+                        </tr>
+                    </thead>
+                   
+                  <?php while ($row = $result->fetch_assoc()): ?>
+                  <tr>  
+                    <td> <?= $row['name']; ?> </td>
+                    <td> <?= $row['location']; ?> </td>
+                    <td> <?= $row['date_established']; ?> </td>
+                    <td> <?= $row['area_in_acres']; ?> </td>
+                    <td> <?= $row['description']; ?> </td>
+                <?php endwhile ?>  
 
-                    
-                       
-                       
-                </table> 
-            </div>
-      </div>
+                
+                   
+                   
+            </table> 
+        </div>
+  </div>
 
-    </div><!-- /.container -->
+</div><!-- /.container -->
 
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="assets/js/bootstrap-datepicker.js">
-    ('.datepicker').datepicker();
-    </script>
-  </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+</body>
 </html>
